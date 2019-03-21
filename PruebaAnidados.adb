cd872cc624320aa21f8787e2552f3814b272a19a
Jose <jrfo_04@unitec.edu>
2019-03-20 22:19:03 -0600

    Pusheando anidados

---
+++ PruebaAnidados.adb
@@ -0,0 +1,33 @@
+procedure Proced is
+
+   Counter : INTEGER;
+
+   procedure Write_A_Header() is
+   begin
+      Counter := 1;
+      Put("This is the heading for this little program.");
+      
+   end Write_A_Header;
+
+   procedure Write_And_Increment() is
+   begin
+      Put("This is line number");
+      
+      Put_Line(" of this program.");
+      Counter := Counter + 1;
+   end Write_And_Increment;
+
+   procedure Write_An_Ending_Statement() is
+   begin
+      Put_Line("This is the end of this little program.");
+   end Write_An_Ending_Statement;
+
+begin
+   Write_A_Header();
+   for Index in 1..7 loop
+      Write_And_Increment();
+      exit when Counter = 6;
+   end loop;
+   Write_An_Ending_Statement();
+end Proced;
+
