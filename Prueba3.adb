3b70e4f00f8fe0a7f66b90f2898e623159cf39f6
hrmendoza96 <hrmendoza96@unitec.edu>
2019-05-22 21:29:30 -0600

    Cambio HashMap a ArrayList. Se agrego la columna atributo

--- Prueba3.adb
+++ Prueba3.adb
@@ -1,13 +1,22 @@
-procedure Nombre is
-  A: Integer := 5;
-  function Factorial() return Integer is
-  begin
-    if A < 0 then                -- Stop recursion if A <= 0.
-      return 1;
-    elsif A = 0 then
-      return 1;
-    else
-      return A * Factorial(A - 1);   -- recurse.
-    end if;
-  end Factorial;
-end Nombre;
+--procedure Nombre is
+  --A: Integer := 5;
+  --function Factorial() return Integer is
+  --begin
+    --if A < 0 then                -- Stop recursion if A <= 0.
+      --return 1;
+    --elsif A = 0 then
+      --return 1;
+    --else
+      --return A * Factorial(A - 1);   -- recurse.
+    --end if;
+  --end Factorial;
+--end Nombre;
+
+function Factorial (N: Integer) return Integer is
+begin
+  if N = 1 then
+    return 1;
+  else
+    return N * Factorial (N - 1);
+  end if;
+end Factorial;
--- TablaSimbolos.java
+++ TablaSimbolos.java
@@ -2,74 +2,114 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
-class Simbolo {  
+class Simbolo {
     String nombre;
     String tipoVariable;
     Object valor;
     Boolean tipoConstante;
     Boolean isFunction;
-    
-    public Simbolo(String nombre, String tipoVariable, Object valor, Boolean tipoConstante, Boolean isFunction) {
-        this.nombre = nombre;       
+    String ambito;
+
+    public Simbolo(String nombre, String tipoVariable, Object valor, Boolean tipoConstante, Boolean isFunction, String ambito) {
+        this.nombre = nombre;
         this.tipoVariable = tipoVariable;
         this.valor = valor;
         this.tipoConstante = tipoConstante;
         this.isFunction = isFunction;
+        this.ambito = ambito;
     }
+
     public Simbolo() {
 
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getTipoVariable() {
+        return this.tipoVariable;
+    }
+
+    public Object getValor() {
+        return this.valor;
+    }
+
+    public Boolean getTipoConstante() {
+        return this.tipoConstante;
+    }
+
+    public Boolean getIsFunction() {
+        return this.isFunction;
     }
 
 }
 
 public class TablaSimbolos {
-    static Map<String, Simbolo> tablaSimbolos = new HashMap();
-    static Stack<String> lista;
-    static ArrayList<Nodo> repeticiones;        
-    
-    public static Logger log = Logger.getLogger(TablaSimbolos.class.getName());            
-    
-    public TablaSimbolos() {        
-        tablaSimbolos = new HashMap<String, Simbolo>();                                   
-        lista = new Stack<String>();                
-    }
-    
+    static ArrayList<Simbolo> tablaSimbolos = new ArrayList<Simbolo>();
+    // static Map<String, Simbolo> tablaSimbolos = new HashMap();
+    // static Stack<String> lista;
+    // static ArrayList<Nodo> repeticiones;
+
+    public static Logger log = Logger.getLogger(TablaSimbolos.class.getName());
+
+    // public TablaSimbolos() {
+    // tablaSimbolos = new HashMap<String, Simbolo>();
+    // lista = new Stack<String>();
+    // }
+
     static public String verificarTipoVariable(String nombre) {
-        Simbolo s = tablaSimbolos.get(nombre);
+        Simbolo s = buscar(nombre);
         return s.tipoVariable;
     }
 
     static public Boolean verificarTipoConstante(String nombre) {
-        Simbolo s = tablaSimbolos.get(nombre);
+        Simbolo s = buscar(nombre);
         return s.tipoConstante;
     }
 
     static public Boolean verificarIsFunction(String nombre) {
-        Simbolo s = tablaSimbolos.get(nombre);
+        Simbolo s = buscar(nombre);
         return s.isFunction;
     }
-    
-    static public String eliminar(String nombre) {             
+
+    static public void setAmbito(String nombre, String ambito){
+        Simbolo s = buscar(nombre);
+        s.ambito  =  ambito;
+        int pos = tablaSimbolos.indexOf(s);
+        tablaSimbolos.set(pos, s);
+        
+    }
+
+    static public String eliminar(String nombre) {
         System.out.println("Eliminando variable: " + nombre);
         try {
-            tablaSimbolos.remove(nombre);
+            Simbolo s = buscar(nombre);
+            tablaSimbolos.remove(s);
             return nombre;
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("Error al eliminar una variable de la tabla de simbolos");
             return nombre;
-        }                
+        }
     }
-            
-    static public Simbolo crear(String nombre, String tipoVariable, Boolean constante, Boolean function) {        
-        Simbolo simbolo = null;    
-        // La variable no existe         
-        if (!tablaSimbolos.containsKey(nombre)) {
-            simbolo = new Simbolo(nombre, tipoVariable, null, constante, function);
+
+    static public Simbolo crear(String nombre, String tipoVariable, Boolean constante, Boolean function) {
+        Simbolo simbolo = null;
+        // Validar si existe
+        boolean existe = false;
+        for (Simbolo s : tablaSimbolos) {
+            if (s.nombre.equals(nombre)) {
+                existe = true;
+                break;
+            }
+        }
+        // La variable no existe
+        if (!existe) {
+            simbolo = new Simbolo(nombre, tipoVariable, null, constante, function, "");
             System.out.println("Agregando a tabla de simbolos con nombre: " + nombre);
-            tablaSimbolos.put(nombre, simbolo);            
-            //System.out.println("Variable creada exitosamente!!!");
-            imprimir();               
+            tablaSimbolos.add(simbolo);
+            // System.out.println("Variable creada exitosamente!!!");
+            imprimir();
             System.out.println("Termino de imprimir");
             return simbolo;
         } else {
@@ -77,18 +117,19 @@ public class TablaSimbolos {
             return null;
         }
     }
-        
+
     static public Simbolo insertar(String nombre, Object valor, Boolean constante) {
         System.out.println("\nIngreso a insertar valor a variable.");
         Simbolo simbolo = buscar(nombre);
-        //La variable existe
-        if(simbolo != null)  {
-            //Actualizar el valor
+        int pos = tablaSimbolos.indexOf(simbolo);
+        // La variable existe
+        if (simbolo != null) {
+            // Actualizar el valor
             simbolo.valor = valor;
             simbolo.tipoConstante = constante;
-            tablaSimbolos.remove(nombre);//Elimino para actualizar
-            tablaSimbolos.put(nombre, simbolo);
-            
+            //tablaSimbolos.remove(nombre);//Elimino para actualizar
+            //tablaSimbolos.put(nombre, simbolo);
+            tablaSimbolos.set(pos, simbolo);
             System.out.println("Variable actualizada");
             imprimir();
             System.out.println("Saliendo de insertar de TablaSimbolos\n");
@@ -96,19 +137,28 @@ public class TablaSimbolos {
         } else
             return null;
     }
-    
+
     static public Simbolo buscar(String nombre) {
-        Simbolo n = (Simbolo)tablaSimbolos.get(nombre); 
-        return n;
+        // Simbolo n = (Simbolo)tablaSimbolos.get(nombre);
+        // Busca si existe la variable en tabla de simbolos
+        Simbolo s = null;
+        for (Simbolo simbolo : tablaSimbolos) {
+            if (simbolo.nombre.equals(nombre)) {
+                s = simbolo;
+                break;
+            }
+        }
+        return s;
     }
-    
+
     static public void imprimir() {
         System.out.println("\nIngresando a imprimir de TablaSimbolos");
         System.out.println("    Valores de la tabla de simbolos:");
-        for (Simbolo s : tablaSimbolos.values())
-            System.out.println(String.format("      "
-                    + "| Nombre: %s | valor: %s | tipoVariable: %s | tipoConstante: %s | Function: %s",s.nombre, s.valor, s.tipoVariable, s.tipoConstante, s.isFunction));        
-        System.out.println("Saliendo de imprimir en TablaSimbolos\n ");        
+        for (Simbolo s : tablaSimbolos)
+            System.out.println(String.format(
+                    "      " + "| Nombre: %s | valor: %s | tipoVariable: %s | tipoConstante: %s | Function: %s | Ambito: %s |",
+                    s.nombre, s.valor, s.tipoVariable, s.tipoConstante, s.isFunction, s.ambito));
+        System.out.println("Saliendo de imprimir en TablaSimbolos\n ");
     }
-    
+
 }
