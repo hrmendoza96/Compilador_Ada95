1c4ee205ed5110fd73b4d482f91d500ce3c20e74
Jose <jrfo_04@unitec.edu>
2019-06-22 00:49:39 -0600

    Pusheando los cambios de codigo final.

--- Prueba1.adb
+++ Prueba1.adb
@@ -1,32 +1,15 @@
-procedure Hello is 
-
-   var: Integer := 4*5+2;
-
+procedure Hello is   
+   varPR: Float;
+   siguiente: Integer := 4;
+   booleano : Boolean;
 begin
-   if 1>=2 or 3<4 then
-      Put("Hola");
-   else
-      Put("Hola2");
-   end if;
-
-   if var>2 and 3<4 then
-      Put("Hola3");
-   else
-      Put("Hola4");
-   end if;
-
-   while 3>2 or 5<7 loop
-      get(var);
-   end loop;
-
-   loop
-      Put("HolaLOOP");
-      exit when 2>1;
-   end loop;
-
-   for i in 1 .. 5 loop
-      Put("HolaFOR");
-   end loop;
-
+   varPR:= 10.1;
+   varPR := varPR + 1.0;
+   siguiente := 10/5 +2;
+   booleano := FALSE;
+   put(booleano);
+   put(siguiente);
+   put("BUenas");
+   put("Tardes");
 end Hello;
 
